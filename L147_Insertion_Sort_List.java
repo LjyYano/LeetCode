32853fbdce759afb9def4ddab8e9753e68bf9ca2
Yano <15822882820@163.com>
2015-12-03 09:31:41 +0800

    LeetCode

---
+++ L147_Insertion_Sort_List.java
@@ -0,0 +1,50 @@
+package LeetCode;
+
+public class L147_Insertion_Sort_List {
+
+	public ListNode insertionSortList(ListNode head) {
+
+		if (head == null)
+			return null;
+		if (head.next == null)
+			return head;
+
+		final ListNode _head = new ListNode(Integer.MIN_VALUE);
+		_head.next = head;
+
+		head = head.next;
+		_head.next.next = null;
+
+		next: while (head != null) {
+
+			ListNode taken = head;
+			head = head.next;
+
+			ListNode cur = _head.next;
+			ListNode last = _head;
+
+			while (cur != null) {
+
+				if (cur.val > taken.val) {
+					// insert
+					last.next = taken;
+					taken.next = cur;
+
+					continue next;
+
+				}
+
+				cur = cur.next;
+				last = last.next;
+			}
+
+			last.next = taken;
+			taken.next = null;
+
+		}
+
+		return _head.next;
+
+	}
+
+}
---
+++ L162_Find_Peak_Element.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+public class L162_Find_Peak_Element {
+
+	public static int findPeakElement(int[] nums) {
+
+		int low = 0;
+		int high = nums.length - 1;
+
+		while (low < high) {
+			int mid = low + (high - low) / 2;
+			if (nums[mid] > nums[mid + 1]) {
+				high = mid;
+			} else {
+				low = mid + 1;
+			}
+		}
+
+		return low;
+	}
+
+}
