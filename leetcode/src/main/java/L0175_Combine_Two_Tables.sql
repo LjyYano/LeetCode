e39a19fe2366d1f74f0ed8c89346f0c133651d56
liujiayu03 <15822882820@163.com>
2020-08-09 08:55:32 +0800

    LeetCode mysql

---
+++ leetcode/src/main/java/L0175_Combine_Two_Tables.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select p.FirstName, p.LastName, a.City, a.State from Person p left join Address a on p.PersonId = a.PersonId;
---
+++ leetcode/src/main/java/L0176_Second_Highest_Salary.sql
@@ -0,0 +1,1 @@
+select (select distinct Salary from Employee order by Salary desc limit 1 offset 1) as SecondHighestSalary
