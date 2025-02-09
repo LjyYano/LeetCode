6dea4553c2c8dae434edebed93b3aa5aff28b6c3
Yano <15822882820@163.com>
2025-02-10 07:58:08 +0800

    update 第十高的薪水

---
+++ src/main/sql/L0195_TenthLine.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  DECLARE M INT;
+  SET M = N - 1;
+  RETURN (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET M
+  );
+END 
