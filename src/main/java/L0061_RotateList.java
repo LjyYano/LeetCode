6963676c59689a3b19ea204021a3a6186eea01f5
Yano <15822882820@163.com>
2025-02-09 10:20:15 +0800

    update 旋转链表：添加示例图片

--- src/main/java/L0061_RotateList.java
+++ src/main/java/L0061_RotateList.java
@@ -4,10 +4,12 @@
  * 给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。
  * 
  * 示例 1：
+ * ![img](https://assets.leetcode.com/uploads/2020/11/13/rotate1.jpg)
  * 输入：head = [1,2,3,4,5], k = 2
  * 输出：[4,5,1,2,3]
  * 
  * 示例 2：
+ * ![img](https://assets.leetcode.com/uploads/2020/11/13/roate2.jpg)
  * 输入：head = [0,1,2], k = 4
  * 输出：[2,0,1]
  * 
